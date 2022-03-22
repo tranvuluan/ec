899d3ec2452b47b871ea47ea1312e0d4eddece9a
CuliDev <tranvuluan069@gmail.com>
2022-03-22 17:41:11 +0700

    1

--- lib/checklogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $path = realpath(dirname(__FILE__));
-
-    require_once($path . '/../class/customer.php');
-
-    $customerModel = new Customer();
-    $username = $_POST['username'];
-?>
